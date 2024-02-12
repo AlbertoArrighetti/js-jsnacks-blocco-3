56e51d18163abed2740c25667b054a07399cb9e7
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-12 14:13:21 +0100

    form and btn function

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,14 @@
 // Scrivi una funzione che accetti una stringa come argomento
 // e la restituisca girata (es. "Ciao" -> "oaiC")
+
+
+// prelevo gli elementi 
+const btnElement = document.getElementById("formBtn");
+const userWord = document.getElementById("formWord");
+
+
+btnElement.addEventListener("click", function(){
+
+    
+
+})
