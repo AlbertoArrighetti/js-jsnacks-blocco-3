d9d4f9e358d908a0f4a085d5026c703f5c2b30be
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-12 15:02:41 +0100

    snack2 - funzione per alternare gli array

---
+++ jsnack2/script.js
@@ -0,0 +1,39 @@
+// Scrivi una funzione che fonda due array presi come parametri (date per scontato che abbiano lo stesso numero di elementi) 
+// prendendo alternativamente gli elementi da uno e dall’altro
+
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+
+// creo 2 array 
+const firstArray = ["a", "b", "c"];
+const secondArray = [1, 2, 3];
+
+
+// riusltato 
+const result = combineArray(firstArray, secondArray);
+console.log(result);
+
+
+
+
+// funzione che abbia i 2 parametri 
+function combineArray(arrOne, arrTwo){
+
+    // creo un array vuoto nel quale inserire i 2 array
+    const arrayCombined = [];
+
+    // inizializzo una variabile a zero per le iterazioni
+    let i = 0;
+
+    // finchè il numero di iterazioni è minore alla lunghezza di uno dei due array (essendo entrambi lunghi uguali)
+    while (i < arrOne.length) {
+
+        // aggiungi gli elementi 
+        arrayCombined.push(arrOne[i], arrTwo[i]);
+        // aumenta le iterazioni 
+        i++;   
+    }
+
+    // ritorna il valore dell'array combinato 
+    return arrayCombined;
+}
