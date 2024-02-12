1d8bca9e7656e039742666e5c946a39b690c9947
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-12 14:27:52 +0100

    funzione per la parola invertita

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -6,9 +6,27 @@
 const btnElement = document.getElementById("formBtn");
 const userWord = document.getElementById("formWord");
 
-
+// al click 
 btnElement.addEventListener("click", function(){
-
     
+    // prelevo il valore inserito nel form
+    const wordToRevert = userWord.value;
 
+    // inverto la parola richiamando la funzione 
+    const reverse = reverseWord(wordToRevert)
+
+    console.log(reverse);
 })
+
+// creo una funzione per invertire le parole inserite
+function reverseWord(word) {
+
+
+    // con .split separo la parola in un array di lettere 
+    // con .reverse inverto l'ordine delle lettere nell'array
+    // con .join le unisco per creare la parola invertita 
+    const reverse = word.split('').reverse().join('');
+
+
+    return reverse;
+}
